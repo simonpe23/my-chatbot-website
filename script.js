465b5117cf9ade12af3ca618aef454e25d7ab11c
Simon Pettersson <tcsimon.p@gmail.com>
2025-01-29 19:30:47 -0800

    Updated script.js to use environment variable for API key

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ async function sendMessage() {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer YOUR_OPENAI_API_KEY",
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+
         },
         body: JSON.stringify({
             model: "gpt-4",
